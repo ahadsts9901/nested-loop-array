9a00faf586834d3b61155a36379781cc65159d04
ahadsts9901 <ahadsts990@gmail.com>
2023-03-30 11:38:32 -0700

    2x2 3x3 added

--- main.js
+++ main.js
@@ -1,6 +1,406 @@
-// add 4
+//2x2 matrix
+
+
+// add 2
+
+function add2(event) {
+    {
+        event.preventDefault()
+    }
+
+
+    let x1 = document.getElementById('x1').value
+    x1 = Number(x1)
+    let x2 = document.getElementById('x2').value
+    x2 = Number(x2)
+    let x3 = document.getElementById('x3').value
+    x3 = Number(x3)
+    let x4 = document.getElementById('x4').value
+    x4 = Number(x4)
+
+    let value = document.getElementById('value').value
+    value = Number(value)
+
+    let result = ""
+
+    let matrix = [
+        [x1, x2],
+        [x3, x4]
+    ]
+
+
+    for (let i = 0; i < matrix.length; i++) {
+
+        for (let j = 0; j < matrix[i].length; j++)
+
+            // result = matrix[i][j] + value
+
+            result += (matrix[i][j] + value) + ", " + ""
+
+        document.getElementById('result2').innerHTML = result
+
+    }
+
+
+}
+
+// subtract 2
+
+
+function subtract2(event) {
+    {
+        event.preventDefault()
+    }
+
+
+    let x1 = document.getElementById('x1').value
+    x1 = Number(x1)
+    let x2 = document.getElementById('x2').value
+    x2 = Number(x2)
+    let x3 = document.getElementById('x3').value
+    x3 = Number(x3)
+    let x4 = document.getElementById('x4').value
+    x4 = Number(x4)
+
+    let value = document.getElementById('value').value
+    value = Number(value)
+
+    let result = ""
+
+    let matrix = [
+        [x1, x2],
+        [x3, x4]
+    ]
+
+
+    for (let i = 0; i < matrix.length; i++) {
+
+        for (let j = 0; j < matrix[i].length; j++)
+
+            // result = matrix[i][j] - value
+
+            result += (matrix[i][j] - value) + ", " + ""
+
+        document.getElementById('result2').innerHTML = result
+
+    }
+
+
+}
+
+// multiply 2
+
+function multiply2(event) {
+    {
+        event.preventDefault()
+    }
+
+    let x1 = document.getElementById('x1').value
+    x1 = Number(x1)
+    let x2 = document.getElementById('x2').value
+    x2 = Number(x2)
+    let x3 = document.getElementById('x3').value
+    x3 = Number(x3)
+    let x4 = document.getElementById('x4').value
+    x4 = Number(x4)
+
+    let value = document.getElementById('value').value
+    value = Number(value)
+
+    let result = ""
+
+    let matrix = [
+        [x1, x2],
+        [x3, x4]
+    ]
+
+
+    for (let i = 0; i < matrix.length; i++) {
+
+        for (let j = 0; j < matrix[i].length; j++)
+
+            // result = matrix[i][j] * value
+
+            result += (matrix[i][j] * value) + ", " + ""
+
+        document.getElementById('result2').innerHTML = result
+
+    }
+
+
+}
+
+// divide 2
+
+function divide2(event) {
+    {
+        event.preventDefault()
+    }
+
+
+    let x1 = document.getElementById('x1').value
+    x1 = Number(x1)
+    let x2 = document.getElementById('x2').value
+    x2 = Number(x2)
+    let x3 = document.getElementById('x3').value
+    x3 = Number(x3)
+    let x4 = document.getElementById('x4').value
+    x4 = Number(x4)
+
+    let value = document.getElementById('value').value
+    value = Number(value)
+
+    let result = ""
+
+    let matrix = [
+        [x1, x2],
+        [x3, x4]
+    ]
+
+
+    for (let i = 0; i < matrix.length; i++) {
+
+        for (let j = 0; j < matrix[i].length; j++)
+
+            // result = matrix[i][j] / value
+
+            result += (matrix[i][j] / value) + ", " + ""
+
+        document.getElementById('result2').innerHTML = result
+
+    }
+
+
+}
+
+
+//3x3 matrix
+
+
+
+// add 3
 
 function add(event) {
+    {
+        event.preventDefault()
+    }
+
+
+    let x1 = document.getElementById('x1').value
+    x1 = Number(x1)
+    let x2 = document.getElementById('x2').value
+    x2 = Number(x2)
+    let x3 = document.getElementById('x3').value
+    x3 = Number(x3)
+    let x4 = document.getElementById('x4').value
+    x4 = Number(x4)
+    let x5 = document.getElementById('x5').value
+    x5 = Number(x5)
+    let x6 = document.getElementById('x6').value
+    x6 = Number(x6)
+    let x7 = document.getElementById('x7').value
+    x7 = Number(x7)
+    let x8 = document.getElementById('x8').value
+    x8 = Number(x8)
+    let x9 = document.getElementById('x9').value
+    x9 = Number(x9)
+
+    let value = document.getElementById('value').value
+    value = Number(value)
+
+    let result = ""
+
+    let matrix = [
+        [x1, x2, x3],
+        [x4, x5, x6],
+        [x7, x8, x9]
+    ]
+
+
+    for (let i = 0; i < matrix.length; i++) {
+
+        for (let j = 0; j < matrix[i].length; j++)
+
+            // result = matrix[i][j] + value
+
+            result += (matrix[i][j] + value) + ", " + ""
+
+        document.getElementById('result').innerHTML = result
+
+    }
+
+
+}
+
+// subtract 3
+
+
+function subtract(event) {
+    {
+        event.preventDefault()
+    }
+
+
+    let x1 = document.getElementById('x1').value
+    x1 = Number(x1)
+    let x2 = document.getElementById('x2').value
+    x2 = Number(x2)
+    let x3 = document.getElementById('x3').value
+    x3 = Number(x3)
+    let x4 = document.getElementById('x4').value
+    x4 = Number(x4)
+    let x5 = document.getElementById('x5').value
+    x5 = Number(x5)
+    let x6 = document.getElementById('x6').value
+    x6 = Number(x6)
+    let x7 = document.getElementById('x7').value
+    x7 = Number(x7)
+    let x8 = document.getElementById('x8').value
+    x8 = Number(x8)
+    let x9 = document.getElementById('x9').value
+    x9 = Number(x9)
+
+    let value = document.getElementById('value').value
+    value = Number(value)
+
+    let result = ""
+
+    let matrix = [
+        [x1, x2, x3],
+        [x4, x5, x6],
+        [x7, x8, x9]
+    ]
+
+
+    for (let i = 0; i < matrix.length; i++) {
+
+        for (let j = 0; j < matrix[i].length; j++)
+
+            // result = matrix[i][j] - value
+
+            result += (matrix[i][j] - value) + ", " + ""
+
+        document.getElementById('result').innerHTML = result
+
+    }
+
+
+}
+
+// multiply 3
+
+function multiply(event) {
+    {
+        event.preventDefault()
+    }
+
+
+    let x1 = document.getElementById('x1').value
+    x1 = Number(x1)
+    let x2 = document.getElementById('x2').value
+    x2 = Number(x2)
+    let x3 = document.getElementById('x3').value
+    x3 = Number(x3)
+    let x4 = document.getElementById('x4').value
+    x4 = Number(x4)
+    let x5 = document.getElementById('x5').value
+    x5 = Number(x5)
+    let x6 = document.getElementById('x6').value
+    x6 = Number(x6)
+    let x7 = document.getElementById('x7').value
+    x7 = Number(x7)
+    let x8 = document.getElementById('x8').value
+    x8 = Number(x8)
+    let x9 = document.getElementById('x9').value
+    x9 = Number(x9)
+
+    let value = document.getElementById('value').value
+    value = Number(value)
+
+    let result = ""
+
+    let matrix = [
+        [x1, x2, x3],
+        [x4, x5, x6],
+        [x7, x8, x9]
+    ]
+
+
+    for (let i = 0; i < matrix.length; i++) {
+
+        for (let j = 0; j < matrix[i].length; j++)
+
+            // result = matrix[i][j] * value
+
+            result += (matrix[i][j] * value) + ", " + ""
+
+        document.getElementById('result').innerHTML = result
+
+    }
+
+
+}
+
+// divide 3
+
+function divide(event) {
+    {
+        event.preventDefault()
+    }
+
+
+    let x1 = document.getElementById('x1').value
+    x1 = Number(x1)
+    let x2 = document.getElementById('x2').value
+    x2 = Number(x2)
+    let x3 = document.getElementById('x3').value
+    x3 = Number(x3)
+    let x4 = document.getElementById('x4').value
+    x4 = Number(x4)
+    let x5 = document.getElementById('x5').value
+    x5 = Number(x5)
+    let x6 = document.getElementById('x6').value
+    x6 = Number(x6)
+    let x7 = document.getElementById('x7').value
+    x7 = Number(x7)
+    let x8 = document.getElementById('x8').value
+    x8 = Number(x8)
+    let x9 = document.getElementById('x9').value
+    x9 = Number(x9)
+
+    let value = document.getElementById('value').value
+    value = Number(value)
+
+    let result = ""
+
+    let matrix = [
+        [x1, x2, x3],
+        [x4, x5, x6],
+        [x7, x8, x9]
+    ]
+
+
+    for (let i = 0; i < matrix.length; i++) {
+
+        for (let j = 0; j < matrix[i].length; j++)
+
+            // result = matrix[i][j] / value
+
+            result += (matrix[i][j] / value) + ", " + ""
+
+        document.getElementById('result').innerHTML = result
+
+    }
+
+
+}
+
+
+// 4x4 matrix
+
+// add 4
+
+function add4(event) {
     {
         event.preventDefault()
     }
@@ -60,7 +460,7 @@ function add(event) {
 
             result += (matrix[i][j] + value) + ", " + ""
 
-        document.getElementById('result').innerHTML = result
+        document.getElementById('result4').innerHTML = result
 
     }
 
@@ -70,7 +470,7 @@ function add(event) {
 // subtract 4
 
 
-function subtract(event) {
+function subtract4(event) {
     {
         event.preventDefault()
     }
@@ -130,7 +530,7 @@ function subtract(event) {
 
             result += (matrix[i][j] - value) + ", " + ""
 
-        document.getElementById('result').innerHTML = result
+        document.getElementById('result4').innerHTML = result
 
     }
 
@@ -139,7 +539,7 @@ function subtract(event) {
 
 // multiply 4
 
-function multiply(event) {
+function multiply4(event) {
     {
         event.preventDefault()
     }
@@ -199,7 +599,7 @@ function multiply(event) {
 
             result += (matrix[i][j] * value) + ", " + ""
 
-        document.getElementById('result').innerHTML = result
+        document.getElementById('result4').innerHTML = result
 
     }
 
@@ -208,7 +608,7 @@ function multiply(event) {
 
 // divide 4
 
-function divide(event) {
+function divide4(event) {
     {
         event.preventDefault()
     }
@@ -268,7 +668,7 @@ function divide(event) {
 
             result += (matrix[i][j] / value) + ", " + ""
 
-        document.getElementById('result').innerHTML = result
+        document.getElementById('result4').innerHTML = result
 
     }
 
